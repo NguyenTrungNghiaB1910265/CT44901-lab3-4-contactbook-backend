e3c3d12da9342d3f313de59829ca6384fff0fefb
ntnghia01 <nguyentrungnghia26112001@gmail.com>
2022-11-03 14:23:08 +0700

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyen cac bien trong $route.params vao lam props
     },
+    {
+        path: "/contacts",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+    },
 ];
 const router = createRouter({ 
     history: createWebHistory(import.meta.env.BASE_URL), 
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -16,9 +16,15 @@
                 <button class="btn btn-sm btn-primary" @click="refreshList()">
                     <i class="fas fa-redo"></i> Làm mới
                 </button>
-                <button class="btn btn-sm btn-success" @click="goToAddContact">
-                    <i class="fas fa-plus"></i> Thêm mới
-                </button>
+                <router-link 
+                    :to="{
+                        name: 'contact.add',
+                    }" 
+                >
+                    <button class="btn btn-sm btn-success" @click="goToAddContact">
+                        <i class="fas fa-plus"></i> Thêm mới
+                    </button>
+                </router-link> 
                 <button class="btn btn-sm btn-danger" @click="removeAllContacts">
                     <i class="fas fa-trash"></i> Xóa tất cả
                 </button>
@@ -36,7 +42,7 @@
                         name: 'contact.edit', 
                         params: { id: activeContact._id }, 
                     }" 
-                > 
+                >
                     <span class="mt-2 badge badge-warning"> 
                         <i class="fas fa-edit"></i> Hiệu chỉnh
                     </span> 
